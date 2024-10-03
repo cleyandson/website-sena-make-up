2b3e3280a1e9de3886c9284f99e383cbb73bc480
viitor14 <manoelvitor550@gmail.com>
2024-10-03 18:28:40 -0300

    Implementação do carrosel e do banner

--- src/main/main.php
+++ src/main/main.php
@@ -1,4 +1,33 @@
 <main>
+    <div class="box-carousel">
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="3000">
+                    <img src="../../../website-sena-make-up/src/main/assets/img/banner-primary.jpg"
+                        class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="5000">
+                    <img src="../../../website-sena-make-up/src/main/assets/img/banner-secondary.jpg"
+                        class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="4000">
+                    <img src="../../../website-sena-make-up/src/main/assets/img/banner-tertiary.jpg"
+                        class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
     <div class="box-main">
         <h2>Descubra as novidades</h2>
         <div class="box-content">
@@ -226,6 +255,12 @@
             </div>
 
 
+        </div>
+    </div>
+
+    <div class="box-banner">
+        <div>
+            <img src="../../../website-sena-make-up/src/main/assets/img/banner-primary.jpg" alt="">
         </div>
     </div>
 </main>
